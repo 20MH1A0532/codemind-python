7d26560297eda3e294afebab9d47f61d8baf56e6
20MH1A0532 <84370718+20MH1A0532@users.noreply.github.com>
2022-11-02 12:08:03 +0530

    n1=int(input()) n2=int(input()) for i in range(n1,n2):     if i>1:         for j in range(2,i):             if i%j==0:                 break         else:             print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,9 +1,9 @@
-a=int(input())
-b=int(input())
-for n in range(a,b+1):
-    if n>1:
-        for i in range(2,n):
-            if n%i==0:
+n1=int(input())
+n2=int(input())
+for i in range(n1,n2):
+    if i>1:
+        for j in range(2,i):
+            if i%j==0:
                 break
         else:
-            print(n)
+            print(i)
